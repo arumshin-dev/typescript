901f4e3c656c09e5228304acf4b01862e0d675f8
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-05-14 13:23:44 +0000

    mini localstorage api

--- index.ts
+++ index.ts
@@ -5,14 +5,14 @@ interface SStorage<T> {
 
 class LocalStorage<T> {
   private storage: SStorage<T> = {}
-  set(key:string, value:T){
+  setItem(key:string, value:T){
     this.storage[key] = value;
   }
-  remove(key:string){
-    delete this.storage[key]
-  }
-  get(key:string):T{
+  getItem(key:string):T{
     return this.storage[key]
+  }
+  clearItem(key:string){
+    delete this.storage[key]
   }
   clear(){
     this.storage = {}
@@ -21,12 +21,20 @@ class LocalStorage<T> {
 }
 
 const stringsStorage = new LocalStorage<string>()
-console.log(stringsStorage.get('name'))//undefined
-console.log(stringsStorage.set('name', 'John'))//undefined
-console.log(stringsStorage.get('name'))//John
+console.log(stringsStorage)//LocalStorage { storage: {} }
+console.log(stringsStorage.getItem('name'))//undefined
+console.log(stringsStorage.setItem('name', 'John'))//undefined
+console.log(stringsStorage.getItem('name'))//John
+console.log(stringsStorage)//LocalStorage { storage: { name: 'John' } }
+console.log(stringsStorage.setItem('test', 'TEST'))//undefined
+console.log(stringsStorage)//LocalStorage { storage: { name: 'John', test: 'TEST' } }
+//stringsStorage.clearItem('name')
+stringsStorage.clear()
+console.log(stringsStorage)//LocalStorage { storage: { name: 'John' } }//LocalStorage { storage: {} }
 
 const booleansStorage = new LocalStorage<boolean>();
-console.log(booleansStorage.get('isLoggedIn'))//undefined
-console.log(booleansStorage.set('isLoggedIn', true))//undefined
-console.log(booleansStorage.get('isLoggedIn'))//true
+console.log(booleansStorage.getItem('isLoggedIn'))//undefined
+console.log(booleansStorage.setItem('isLoggedIn', true))//undefined
+console.log(booleansStorage.getItem('isLoggedIn'))//true
+console.log(booleansStorage)//LocalStorage { storage: { isLoggedIn: true } }
 
